c538f404bd2f35c8e36529747eab070f4a6bf591
jty <vividjty@gmail.com>
2025-02-26 21:26:57 +0900

    token 생성

--- src/main/java/websocket/project/spring/member/controller/MemberController.java
+++ src/main/java/websocket/project/spring/member/controller/MemberController.java
@@ -1,6 +1,7 @@
 package websocket.project.spring.member.controller;
 
 import lombok.AllArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -8,18 +9,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import websocket.project.spring.member.common.auth.JwtTokenProvider;
 import websocket.project.spring.member.domain.Member;
+import websocket.project.spring.member.dto.MemberLoginReqDto;
 import websocket.project.spring.member.dto.MemberSaveReqDto;
 import websocket.project.spring.member.service.MemberService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/member")
 public class MemberController {
 
+    private final JwtTokenProvider jwtTokenProvider;
+    private final  MemberService memberService;
 
-    private MemberService memberService;
-    public MemberController(MemberService memberService) {
+    public MemberController(MemberService memberService, JwtTokenProvider jwtTokenProvider) {
         this.memberService = memberService;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @PostMapping("/create")
@@ -28,5 +36,16 @@ public class MemberController {
         return new ResponseEntity<>(member.getId(), HttpStatus.CREATED);
     }
 
+    @PostMapping("/doLogin")
+    public ResponseEntity<?> doLogin(@RequestBody MemberLoginReqDto memberLoginReqDto) throws IllegalAccessException {
+//        email, password 검증
+        Member member = memberService.login(memberLoginReqDto);
+//        일치할 경우 access 토큰 발행
+        String jwtToken = jwtTokenProvider.createToken(member.getEmail(), member.getRole().toString());
+        Map<String, Object> loginInfo = new HashMap<>();
+        loginInfo.put("id", member.getId());
+        loginInfo.put("token", jwtToken);
+        return new ResponseEntity<>(loginInfo, HttpStatus.OK);
+    }
 
 }
