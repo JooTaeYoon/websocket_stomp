b8219d3808b159ea06f4d40797a933c0420d435b
jty <vividjty@gmail.com>
2025-03-08 21:57:59 +0900

    stomp 설정 및 컨트롤러

---
+++ src/main/java/websocket/project/spring/member/chat/config/StompWebSocketConfig.java
@@ -0,0 +1,34 @@
+package websocket.project.spring.member.chat.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/connect").setAllowedOrigins("http://localhost:3000")
+//                http:// 프로토콜도 사용 하게 할 수 있게 해주는 SockJS 라이브러리를 허용해주는 설정
+                .withSockJS();
+    }
+
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+//        /publish/1 형태로 메세지 발행해야 함을 설정
+//        /publish로 시작하는 url패턴으로 메시지가 발행되면 @Controller 객체의 @MessageMapping메서드로 라우팅
+        registry.setApplicationDestinationPrefixes("/publish");
+
+//        /topic/1 형태로 메세지를 수신(subscribe)해야 함을 설정
+//        registry.enableStompBrokerRelay("/topic");
+        registry.enableSimpleBroker("/topic");
+
+    }
+
+}
