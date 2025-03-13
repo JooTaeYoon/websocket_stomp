0bd0048d44a8022db69c72b20e5b310218c6740f
jty <vividjty@gmail.com>
2025-03-13 16:30:38 +0900

    메세지 읽음 처리

--- src/main/java/websocket/project/spring/member/chat/service/ChatService.java
+++ src/main/java/websocket/project/spring/member/chat/service/ChatService.java
@@ -2,6 +2,7 @@ package websocket.project.spring.member.chat.service;
 
 import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import websocket.project.spring.member.chat.domain.ChatMessage;
@@ -17,6 +18,7 @@ import websocket.project.spring.member.domain.Member;
 import websocket.project.spring.member.repository.MemberRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -41,28 +43,50 @@ public class ChatService {
     public void saveMessage(Long roomId, ChatMessageReqDto chatMessageReqDto) {
 //        채팅방 조회
         ChatRoom chatRoom = chatRoomRepository.findById(roomId).orElseThrow(() ->
-            new EntityNotFoundException("room cannot find !")
+                new EntityNotFoundException("room cannot find !")
         );
 //        보낸 사람 조회
         Member sender = memberRepository.findByEmail(chatMessageReqDto.getSenderEmail()).orElseThrow(() -> new EntityNotFoundException("이메일어 없네"));
 
 //        메세지 저장
         ChatMessage chatMessage = ChatMessage.builder().
-            chatRoom(chatRoom).member(sender)
+                chatRoom(chatRoom).member(sender)
                 .content(chatMessageReqDto.getMessage())
                 .build();
-        chatRoomRepository.save(chatRoom);
+//        chatRoomRepository.save(chatRoom);
+        chatMessageRepository.save(chatMessage);
+
 //        사용자별로 읽음여부 저장
         List<ChatParticipant> chatParticipants = chatParticipantRepository.findByChatRoom(chatRoom);
         for (ChatParticipant c : chatParticipants) {
             ReadStatus readStatus = ReadStatus.builder().
-                    chatRoom(chatRoom).member(c.getMember()).chatMessage(chatMessage)
+                    chatRoom(chatRoom)
+                    .member(c.getMember())
+                    .chatMessage(chatMessage)
                     .isRead(c.getMember().equals(sender))
                     .build();
+
             readStatusRepository.save(readStatus);
         }
-
     }
 
+    public void createGroupRoom(String chatRoomName) {
+        log.info("이거 뭐야 : " + SecurityContextHolder.getContext().getAuthentication().getName());
+        Member member = memberRepository.findByEmail(SecurityContextHolder.getContext().getAuthentication().getName())
+                .orElseThrow(() -> new EntityNotFoundException("이메일이 없네"));
+
+//        채팅방 생성
+        ChatRoom chatRoom = ChatRoom.builder().
+                name(chatRoomName).isGroupChat("Y")
+                .build();
+
+//        채팅 참여자로 개설자를 추가
+        chatRoomRepository.save(chatRoom);
+
+        ChatParticipant chatParticipant = ChatParticipant.builder()
+                .chatRoom(chatRoom).member(member)
+                .build();
+        chatParticipantRepository.save(chatParticipant);
+    }
 
 }
